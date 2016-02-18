841d7f849064e2c1862b1e544d3c1c29b1e4eb65
Nikita Tsyhanenko <runninzero@gmail.com>
2016-02-18 20:11:59 +0300

    layout for second slide

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-﻿
+﻿$(function () {
+    var boCat = $('.bo'),
+        loCat = $('.lo'),
+        boOpen = $('.bo .text'),
+        loOpen = $('.lo .text'),
+        boDesc = $('.bo-desc'),
+        loDesc = $('.lo-desc'),
+        boClose = $('.bo-desc .close'),
+        loClose = $('.lo-desc .close');
+
+    boDesc.hide();
+    loDesc.hide();
+
+    boOpen.on('click', function() {
+        boDesc.show();
+        loDesc.hide();
+    });
+
+    loOpen.on('click', function () {
+        loDesc.show();
+        boDesc.hide();
+    });
+
+    boClose.on('click', function () {
+        boDesc.hide();
+    });
+
+    loClose.on('click', function () {
+        loDesc.hide();
+    });
+});
