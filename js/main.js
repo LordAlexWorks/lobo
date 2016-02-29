b01130c7acff5908368f0cc0276c00a522830920
Nikita Tsyhanenko <runninzero@gmail.com>
2016-02-29 19:32:04 +0300

    animation for cats

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 ﻿$(function () {
-    var boCat = $('.bo'),
-        loCat = $('.lo'),
+    var boCat = $('.bo img'),
+        loCat = $('.lo img'),
         boOpen = $('.bo .text'),
         loOpen = $('.lo .text'),
         boDesc = $('.bo-desc'),
@@ -27,5 +27,69 @@
 
     loClose.on('click', function () {
         loDesc.hide();
+    });
+
+
+    
+
+    var Animation = function(element, options) {
+        var self = this;
+
+        self.target = element;
+        self.top = options.top;
+        self.left = options.left;
+        self.time = options.time;
+
+        self.lastTop = 0;
+        self.lastLeft = 0;
+
+        self.cycle = function() {
+            self.lastTop += self.top.delta;
+            self.lastLeft += self.left.delta;
+
+            if (self.lastTop >= self.top.high || self.lastTop <= self.top.low) {
+                self.top.delta *= -1;
+            }
+
+            if (self.lastLeft >= self.left.high || self.lastLeft <= self.left.low) {
+                self.left.delta *= -1;
+            }
+
+            self.target.animate({ top: self.lastTop, left: self.lastLeft }, self.time);
+
+            setTimeout(self.cycle, self.time);
+        };
+
+        self.cycle();
+
+        return self;
+    };
+
+    var boAnim = new Animation(boCat, {
+        top: {
+            low: -80,
+            high: 50,
+            delta: 1
+        },
+        left: {
+            low: -10,
+            high: 70,
+            delta: -0.5
+        },
+        time: 50
+    });
+
+    var loAnim = new Animation(loCat, {
+        top: {
+            low: -30,
+            high: 30,
+            delta: -1
+        },
+        left: {
+            low: -70,
+            high: 20,
+            delta: -0.5
+        },
+        time: 50
     });
 });
