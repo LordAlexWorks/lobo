021a7e68b592d054f170732ae6f17b7322c6d471
Nikita Tsyhanenko <runninzero@gmail.com>
2016-03-09 13:03:42 +0300

    gradient animation for first slide

--- js/main.js
+++ js/main.js
@@ -95,6 +95,10 @@
     var activeSlide;
     var overflowTimer = undefined;
 
+    var weSlideIndex = 1;
+    var themSlideIndex = 2;
+    var usSlideIndex = 3;
+
     var initSlider = function() {
         activeSlide = 0;
         $('.left-arrow').hide();
@@ -149,23 +153,79 @@
     initSlider();
 
     $('.marked.we').on('click', function() {
-        activeSlide = 1;
+        activeSlide = weSlideIndex;
 
         checkArrows();
         activateSlide();
     });
 
     $('.marked.them').on('click', function() {
-        activeSlide = 2;
+        activeSlide = themSlideIndex;
 
         checkArrows();
         activateSlide();
     });
 
     $('.marked.us').on('click', function () {
-        activeSlide = 3;
+        activeSlide = usSlideIndex;
 
         checkArrows();
         activateSlide();
     });
+
+    /* GRADIENT */
+
+    var colors = new Array(
+      [62, 35, 255],
+      [60, 255, 60],
+      [255, 35, 98],
+      [45, 175, 230],
+      [255, 0, 255],
+      [255, 128, 0]);
+
+    var step = 0;
+    var colorIndices = [0, 1, 2, 3];
+    var gradientSpeed = 0.003;
+
+    function updateGradient() {
+
+        if ($ === undefined) return;
+
+        var c0_0 = colors[colorIndices[0]];
+        var c0_1 = colors[colorIndices[1]];
+        var c1_0 = colors[colorIndices[2]];
+        var c1_1 = colors[colorIndices[3]];
+
+        var istep = 1 - step;
+        var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+        var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+        var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+        var color1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
+
+        var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+        var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+        var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+        var color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
+
+        $('.slide-one').css({
+            background: "-webkit-gradient(linear, left top, right top, from(" + color1 + "), to(" + color2 + "))"
+        }).css({
+            background: "-moz-linear-gradient(left, " + color1 + " 0%, " + color2 + " 100%)"
+        });
+
+        step += gradientSpeed;
+        if (step >= 1) {
+            step %= 1;
+            colorIndices[0] = colorIndices[1];
+            colorIndices[2] = colorIndices[3];
+
+            //pick two new target color indices
+            //do not pick the same as the current one
+            colorIndices[1] = (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+            colorIndices[3] = (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+
+        }
+    }
+
+    setInterval(updateGradient, 10);
 });
